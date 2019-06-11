61694fe8108548785b185dc866280a7f13739efc
Julia Ogris <julia.ogris@gmail.com>
2019-06-11 18:54:26 +1000

    Serve data.json as static file
    Move `stub_data/api/chart/go-course-ca39e2f201323a18` to `public/data.json` and
    update `index.js` accordingly.
    Remove `stub_data` directory and all directives to serve and proxy stubbed
    content.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,7 @@ import "tachyons/css/tachyons.css"
 import * as serviceWorker from "./serviceWorker"
 
 // TODO(juliaogris): retrieve url from somewhere reasonable else ;)
-ReactDOM.render(
-  <App url="api/chart/go-course-ca39e2f201323a18" />,
-  document.getElementById("root")
-)
+ReactDOM.render(<App url="data.json" />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
